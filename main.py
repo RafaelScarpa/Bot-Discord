1b24e9e6b5f525d994db386bb46169d2f2cae4bc
Rafael Scarpa <rafa.scarpa.fernandes@gmail.com>
2021-02-23 22:28:28 +0000

    Mais comandos de link, que têm embed. O site mudou para modo escuro. Mais coisas que têm haver com isso.

--- main.py
+++ main.py
@@ -25,45 +25,52 @@ async def on_message(message):
 	canal = message.channel
 	async def enviar(Entrada):
 		await message.channel.send(Entrada)
+	async def enviar2(Entrada):
+		await message.channel.send(embed=Entrada)
 
-	#Impedir loops infinitos.
-	if message.author == client.user:
-		return
+	#Lista de comandos.
+	LstCmnds=discord.Embed(color=0x00ffe1, title='Comandos:',description=(
+	 '**"Comandos do Bot?"** (Esta lista.)' +
+	 '\n**"Hora da Live?"** (Programação padrão do canal.)' +
+	 '\n**"+YouTube"** (Link para o canal do YouTube.)' +
+	 '\n**"+Sobre"** (Info do bot.)' +
+	 '\n**"+Live"** (Link para a live, se estiver acontecendo.)' +
+	 '\n**"+Doação"** (Link para me dar dinheiro.)' +
+	 '\n**"+Avatar"** (Avatar de alguém.)' +
+	 '\n**"+Clear"** (Apaga 30 mensagens. Apenas mods.)' +
+	 '\n**"+Silenciar"** (Silencia um usuário. Apenas mods.)'))
 
-	#Comando para ignorar comandos.
-	if "[ign]" in mensagem:
+	#Impedir loops infinitos e ignorar mensagens.
+	if autor == client.user or "[ign]" in mensagem:
 		return
 
 	#Comando da lista de comandos.
-	if (('comando' in mensagem) and ('bot' in mensagem)) or mensagem == '+comandos':
-		EMBED1 = discord.Embed(title='Comandos:',description=(
-		  '**"Comandos do Bot?"** (Esta lista.)' +
-		  '\n**"Hora da Live?"** (Programação padrão do canal.)' +
-		  '\n**"+YouTube"** (Link para o canal do YouTube.)' +
-		  '\n**"+Live"** (Link para a live, se estiver acontecendo.)' +
-		  '\n**"+Avatar"** (Avatar de alguém.)' +
-		  '\n**"+Sobre"** (Info do bot.)' +
-		  '\n**"+Clear"** (Apaga 30 mensagens. Apenas mods.)' +
-		  '\n**"+Silenciar"** (Não funciona ainda. Apenas mods.)'))
-		await message.channel.send(embed=EMBED1)
+	if ('comando' in mensagem and 'bot' in mensagem) or mensagem=='+comandos':
+		await enviar(LstCmnds)
 
 	#Comando 1
-	if (mensagem == '+youtube') or (mensagem == '+live') or ('[btlnk]' in mensagem):
-		await enviar('https://youtube.co/RafaelScarpa/live')
-	if mensagem == '+twitch':
-		await enviar('https://twitch.tv/Rafael_Scarpa')
+	if mensagem=='+youtube' or mensagem=='+live' or ('[btlnk]' in mensagem):
+		EMBED1=discord.Embed(title="Rafael Scarpa no YouTube", url="https://youtube.co/RafaelScarpa/live", color=0x00ffe1)
+		await enviar2(EMBED1)
 	if mensagem == '+twitter':
-		await enviar('https://twitter.com/ScarpaYT')
+		EMBED1=discord.Embed(title="Rafael Scarpa no Twitter", url="https://twitter.com/ScarpaYT", color=0x00ffe1)
+		await enviar2(embed=EMBED1)
+	if mensagem == '+doação':
+		EMBED1=discord.Embed(title="Doações?! :eyes:", url="https://streamelements.com/rafaelscarpa/tip", color=0x00ffe1)
+		await enviar2(embed=EMBED1)
+	if mensagem == '+links':
+		EMBED1=discord.Embed(title="Website secreto?! :flushed:", url="https://Bot-Discord.rafaelscarpa.repl.co", color=0x00ffe1)
+		await enviar2(EMBED1)
 
 	#Comando 2
-	if ('live' in mensagem) and ('hora' in mensagem):
+	if 'live' in mensagem and 'hora' in mensagem:
 		await enviar('Normalmente, 15:00(BRT/GMT-3) na sexta.')
 
 	#Comando 3
 	if mensagem == '+clear':
 		if autor.permissions_in(canal).manage_messages:
 			await canal.purge(limit=31)
-			await enviar('30 mensangens apagadas por {0.display_name}.'.format(autor))
+			await canal.send('30 mensangens apagadas por {0}.'.format(autor.display_name), delete_after=23)
 		else:
 			await enviar('Você não tem as permissões certas.')
 
@@ -87,7 +94,7 @@ async def on_message(message):
 		)
 	elif mensagem.startswith('+silenciar'):
 		if autor.permissions_in(canal).manage_messages:
-			if (len(message.mentions) == 1):
+			if len(message.mentions)== 1:
 				try:
 					cargo = autor.guild.get_role(800783416752472095)
 					await message.mentions[0].add_roles(cargo)
@@ -96,9 +103,8 @@ async def on_message(message):
 				except:
 					await enviar('Eu acho que deu certo.')
 			else:
-				await enviar('Algo deu errado. Para mais informações, digite "+silenciar".')
+				await enviar('Erro de digitação. Tente novamente.')
 		else:
 			await enviar('Você não tem as permissões certas.')
-
 
 client.run(os.getenv('TOKEN'))
